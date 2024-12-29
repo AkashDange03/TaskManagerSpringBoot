897474bd502a8919d3638e5bb2199fba07077e14
Akash_03 <akash.dange0307@gmail.com>
2024-12-29 20:24:40 +0530

    Initial setup using spring initializer

---
+++ src/main/java/com/akash/taskmanager_spring_app/TaskmanagerSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.akash.taskmanager_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akash/taskmanager_spring_app/TaskmanagerSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.taskmanager_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
